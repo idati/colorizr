2df2e002b3dc024ce8d77a73d80e00b4f1c44b26
idadi <idadi@github.com>
2016-05-07 20:01:47 +0200

    reopen module Colorize::ClassMethods and redefine new color_codes

---
+++ lib/colorizr.rb
@@ -0,0 +1,35 @@
+require 'colorize'
+
+module Colorize::ClassMethods
+
+	def color_codes
+      {
+        :black   => 0,
+        :red     => 1,
+        :green   => 2,
+        :yellow  => 3,
+        :blue    => 4,
+        :pink 	=> 5,
+        :white   => 7,
+        :light_blue     => 64,
+        :light_grey     => 60, 
+        :default => 9
+      }
+    end
+end
+
+class String
+	color_methods
+  	modes_methods
+end
+
+
+puts "light_grey".light_grey
+puts "light_blue".light_blue
+puts "white".white
+puts "pink".pink
+puts "blue".blue
+puts "yellow".yellow
+puts "green".green
+puts "red".red
+puts "black".black
