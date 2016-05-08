82afe7a5a1473c72110e07097c0eebe2a55c038a
idati <specht.hartweger@hotmail.de>
2016-05-08 07:35:36 +0200

    used correct color codes

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require 'colorize'
 module Colorize::ClassMethods
       def color_codes
@@ -46,3 +47,41 @@ p "light_blue".light_blue
 p String.colors
 
 String.sample_colors
+||||||| merged common ancestors
+=======
+require 'colorize'
+
+module Colorize::ClassMethods
+
+	def color_codes
+      {
+        :black   => 0,
+        :red     => 1,
+        :green   => 2,
+        :yellow  => 3,
+        :blue    => 4,
+        :pink 	=> 5,
+        :white   => 7,
+        :light_blue     => 64,
+        :light_grey     => 60, 
+        :default => 9
+      }
+    end
+end
+
+class String
+	color_methods
+  	modes_methods
+end
+
+
+puts "light_grey".light_grey
+puts "light_blue".light_blue
+puts "white".white
+puts "pink".pink
+puts "blue".blue
+puts "yellow".yellow
+puts "green".green
+puts "red".red
+puts "black".black
+>>>>>>> 2df2e002b3dc024ce8d77a73d80e00b4f1c44b26
